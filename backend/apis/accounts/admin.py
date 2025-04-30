e274b17853ceb44c6016d814287c1c7a41672493
Priyans1727C <sbhola833@gmail.com>
2025-04-30 19:18:53 +0530

    refactor: enhance CustomUserAdmin with fieldsets and search functionality

--- backend/apis/accounts/admin.py
+++ backend/apis/accounts/admin.py
@@ -2,12 +2,31 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User, UserProfile
+from django.utils.translation import gettext_lazy as _
 
 class CustomUserAdmin(BaseUserAdmin):
     add_form = UserCreationForm
     form = UserChangeForm
     model = User
     list_display = ['id','username', 'email', 'role', 'status', 'is_active']
+    
+    # Customize fieldsets to match your User model fields
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('email',)}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'role', 'status', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
 
 admin.site.register(User, CustomUserAdmin)
 
